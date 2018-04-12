41423662a44bb37079de148d3d863fd124e4de28
XXuanTW <30396994+XXuanTW@users.noreply.github.com>
2018-04-12 19:05:07 +0800

    Xuan_0412_3
    New
    ListView
    Serachlist

--- app/src/main/java/com/example/user/checkticketproject/DataViewActivity.java
+++ app/src/main/java/com/example/user/checkticketproject/DataViewActivity.java
@@ -2,19 +2,19 @@ package com.example.user.checkticketproject;
 
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
@@ -29,15 +29,18 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-
-
+import java.util.HashMap;
 
 
 public class DataViewActivity extends AppCompatActivity {
     Toolbar toolbar;
     DrawerLayout dataviewlayout;
     NavigationView liftmenu;
-    ListView dateviewlist;
+    ListView dataviewlist;
+    Button serachbutton;
+    Button refresh;
+    EditText textphone;
+    ArrayList<HashMap<String, String>> arrayList;
     int sublist;
     String url ="http://10.0.2.2/Sublist.php";
     @Override
@@ -47,10 +50,27 @@ public class DataViewActivity extends AppCompatActivity {
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         dataviewlayout = (DrawerLayout)findViewById(R.id.dataview_layout);
         liftmenu = (NavigationView)findViewById(R.id.liftmenu);
-        dateviewlist = (ListView)findViewById(R.id.dateviewlist);
+        dataviewlist = (ListView)findViewById(R.id.dataviewlist);
+        serachbutton = (Button)findViewById(R.id.serachphone);
+        refresh = (Button)findViewById(R.id.refresh);
+        textphone = (EditText)findViewById(R.id.textphone);
         new TransTask().execute(url);
+        new TransTaskList().execute("http://10.0.2.2/Sublist.php");
         settoolbar();
         setliftmenu();
+
+        serachbutton.setOnClickListener(new Button.OnClickListener(){
+          public void onClick(View view){
+              new TransTaskList().execute("http://10.0.2.2/Serachlist.php?phone="+textphone.getText().toString());
+          }
+        });
+
+        refresh.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View view){
+                new TransTaskList().execute("http://10.0.2.2/Sublist.php");
+            }
+        });
+
     }
 
     //Json 解析
@@ -103,15 +123,99 @@ public class DataViewActivity extends AppCompatActivity {
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-
         }
     }
-    
+    //List Json
+
+    class TransTaskList extends AsyncTask<String, Void, String> {
+
+
+        private ArrayList<HashMap<String, String>> arrayList;
+        private ListView list_item;
+
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder sb = new StringBuilder();
+
+            try {
+
+                URL url = new URL(params[0]);
+                BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+                String line = in.readLine();
+                while (line != null) {
+
+                    sb.append(line);
+                    line = in.readLine();
+                }
+            } catch (Exception e) {
+
+            }
+
+            return sb.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            parseJSON(s);
+        }
+
+    }
+
+
+    private void parseJSON(String jsonStr) {
+        arrayList = new ArrayList<HashMap<String, String>>();
+        HashMap hashMap_title = new HashMap<String, String>();
+        hashMap_title.put("id", "id");
+        hashMap_title.put("username", "username");
+        hashMap_title.put("ticket", "ticket");
+        hashMap_title.put("phone", "phone");
+
+        arrayList.add(hashMap_title);
+
+        try {
+            StringBuilder sb = new StringBuilder();
+            JSONArray jsonArray = new JSONArray(jsonStr);
+            if (jsonArray != null && jsonArray.length() > 0) {
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    HashMap<String, String> hashMap = new HashMap<>();
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    hashMap.put("id", jsonObject.getString("id"));
+                    hashMap.put("username", jsonObject.getString("username"));
+                    hashMap.put("ticket", jsonObject.getString("ticket"));
+                    hashMap.put("phone", jsonObject.getString("phone"));
+
+                    arrayList.add(hashMap);
+
+
+                }
+            }
+
+            SimpleAdapter simpleAdapter = new SimpleAdapter(
+                    this,
+                    arrayList,
+                    R.layout.data_list,
+                    new String[]{"id", "username", "ticket", "phone"},
+                    new int[]{R.id.textView, R.id.textView2, R.id.textView3, R.id.textView4}
+            );
+
+
+            dataviewlist.setAdapter(simpleAdapter);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
     private void settoolbar() {
         toolbar.setTitle("查詢系統");
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
+                new TransTaskList().execute("http://10.0.2.2/Sublist.php");
                 return true;
             }
         });
